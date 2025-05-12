0d6f495a2487debad3090970e7044937882f758c
Wiberg, Gustav Karl Henrik (DCBP) <gustav.wiberg@unibe.ch>
2025-05-12 13:02:44 +0200

    fixing paths

--- Copy2Arduino.bat
+++ Copy2Arduino.bat
@@ -5,15 +5,17 @@ set Folder=MassFlow_Sensirion_SFC6xxxx
 
 echo %Folder%
 set GitHub=%UserProfile%\Documents\GitHub
-set SRCPath=%UserProfile%\Documents\GitHub\%Folder%
-set SRCPath_SRC=%SRCPath%\src
-set SRCPath_EXA=%SRCPath%\examples
+
+::set SRCPath=%UserProfile%\Documents\GitHub\%Folder%
+set SRCPath=%~dp0
+set SRCPath_SRC=%SRCPath%src
+set SRCPath_EXA=%SRCPath%examples
 set ArduinoLibPath=%UserProfile%\Documents\Arduino\libraries\%Folder%
 set ArduinoLibPath_SRC=%ArduinoLibPath%\src
 set ArduinoLibPath_EXA=%ArduinoLibPath%\examples
 
 ::
-echo %SRCPath%
+echo SRC_PATH: %SRCPath%
 ::
 mkdir %ArduinoLibPath%
 mkdir %ArduinoLibPath_SRC%
@@ -27,9 +29,9 @@ echo %ArduinoLibPath_SRC%
 echo %SRCPath%
 :: %ArduinoLibPath_SRC%\*.*
 ::
-echo "copying: %SRCPath%\lib*.*"
-copy "%SRCPath%\lib*.*" "%ArduinoLibPath%"
-copy "%SRCPath_SRC%\*.*" "%ArduinoLibPath_SRC%"
+echo "copying: %SRCPath%lib*.*"
+copy "%SRCPath%lib*.*" "%ArduinoLibPath%"
+xcopy "%SRCPath_SRC%" "%ArduinoLibPath_SRC%" /E /Y
 ::copy "%SRCPath_SRC%\server\*.*" "%ArduinoLibPath_SRC%\server"
 ::copy "%SRCPath_SRC%\util\*.*" "%ArduinoLibPath_SRC%\util"
 ::copy "%SRCPath_SRC%\com\*.*" "%ArduinoLibPath_SRC%\com"
